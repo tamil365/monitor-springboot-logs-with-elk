3075e187ce1608aa63ec249aff79e661071f83f8
balajich <balaji.chopparapu@gmail.com>
2020-07-12 21:27:55 +0530

    added exception condition

--- springbootrestservice/src/main/java/org/eduami/restservice/GreetingController.java
+++ springbootrestservice/src/main/java/org/eduami/restservice/GreetingController.java
@@ -1,19 +1,22 @@
 package org.eduami.restservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        if (name.equalsIgnoreCase("devil")) {
+            throw new RuntimeException("I am scared");
+        }
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
